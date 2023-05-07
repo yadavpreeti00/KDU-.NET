59ba808bd7420d908d1448d2c51c3fbc3cc734ee
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-05-08 02:41:34 +0530

    exercise_2 done

---
+++ exercise_2/exercise_2/TrackHighScore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_2
+{
+    internal class TrackHighScore
+    {
+        int highScore = 100;
+        string highScorePlayer = "Preeti";
+
+        public void UpdateHighScore(int score, string playerName)
+        {
+            if (score > highScore)
+            {
+                Console.WriteLine("New highscore is " + score);
+                Console.WriteLine("New highscore holder is " + playerName);
+                highScore = score;
+                highScorePlayer = playerName;
+            }
+            else
+            {
+                Console.WriteLine("The old highscore of " + highScore + " could not be broken and is still held by " + highScorePlayer);
+            }
+        }
+    }
+}
