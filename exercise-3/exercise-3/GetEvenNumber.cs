0af2873758be31663133f86fa025f2e3685fc688
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-05-08 03:29:56 +0530

    exercise-3 done

---
+++ exercise-3/exercise-3/GetEvenNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercise_3
+{
+    internal class GetEvenNumber
+    {
+        public List<int> GetEvenNumbersInRange(int start,int end)
+        {
+            List<int> evenNumbers = new List<int>();
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbers.Add(i);
+                }
+            }
+            return evenNumbers;
+        }
+    }
+}
---
+++ exercise-3/exercise-3/Program.cs
@@ -0,0 +1,25 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace exercise_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           CountOccurences countOccurences = new CountOccurences();
+           countOccurences.GetOccurencesCount();
+
+           GetEvenNumber getEvenNumber=new GetEvenNumber();
+            List<int> evenNumbers = getEvenNumber.GetEvenNumbersInRange(100, 170);
+           foreach (int num in evenNumbers)
+           {
+               Console.WriteLine(num);
+           }
+
+        }
+    }
+}
+
