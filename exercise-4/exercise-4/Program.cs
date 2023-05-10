6676cfbce09fd60ed0ef1d6710d79d7648500402
yadavpreeti00 <yadavpreeti5668@gmail.com>
2023-05-10 10:02:45 +0530

    exercise-4 done

---
+++ exercise-4/exercise-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace exercise_4 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RotateArray rotateArray = new RotateArray();
+            rotateArray.RotateGivenArray();
+            ConvertToDictionary convertToDictionary = new ConvertToDictionary();
+            convertToDictionary.MakeDictionaryFromArrays();
+
+        }
+    }
+}
